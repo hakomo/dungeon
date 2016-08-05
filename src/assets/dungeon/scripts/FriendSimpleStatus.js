38c23ee38ad0458acf1024dffee2d05f289f9446
Takumi Funasaka <hakomof@gmail.com>
2016-08-05 23:51:01 +0900

    Add FriendSimpleStatus class

---
+++ src/assets/dungeon/scripts/FriendSimpleStatus.js
@@ -0,0 +1,23 @@
+
+class FriendSimpleStatus {
+    constructor(game, x, y) {
+        this.cont = game.add.graphics(x, y)
+            .lineStyle(1, COLOR_WHITE)
+            .drawRect(2, 19, 81, 5)
+        this.bar = game.make.graphics(3, 20)
+            .beginFill(COLOR_GREEN)
+            .drawRect(0, 0, 80, 4)
+        this.cont.addChild(this.bar)
+        this.char = this.cont.addChild(game.make.text(0, 0, '', FONT_CYAN))
+        this.name = this.cont.addChild(game.make.text(23, 3, '', FONT_VERDANA))
+        this.hide()
+    }
+
+    show(friend) {
+        this.cont.visible = true
+    }
+
+    hide() {
+        this.cont.visible = false
+    }
+}
