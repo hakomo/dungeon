034a80e417ae289965fa95ed2f2c86de67d8b64d
hakomo <hakomof@gmail.com>
2016-08-28 03:02:41 +0900

    Animate bar

--- src/dungeon/scripts/Bar.js
+++ src/dungeon/scripts/Bar.js
@@ -1,13 +1,30 @@
 
 class Bar {
     constructor(game, x, y, width, height) {
+        this.game = game
         this.width = width
         this.cont = game.make.graphics(x, y)
             .beginFill(COLOR_GREEN)
             .drawRect(0, 0, width, height)
+        this.tween = null
     }
 
     change(prop) {
+        if (this.tween) {
+            this.tween.stop()
+            this.tween = null
+        }
         this.cont.width = Math.ceil(prop * this.width)
+    }
+
+    animate(prop) {
+        if (this.tween) {
+            this.tween.stop()
+            this.tween = null
+        }
+        let width = Math.ceil(prop * this.width)
+        if (width === this.cont.width) return
+        this.tween = this.game.add.tween(this.cont).to({ width },
+            Math.abs(width - this.cont.width) * 20, null, true)
     }
 }
