f2919630b49aa1ad6e18a754fb218c0a5ac96b10
hakomo <hakomof@gmail.com>
2016-09-12 20:32:53 +0900

    Effect auto healing to all enemies

--- src/dungeon/scripts/Effect.js
+++ src/dungeon/scripts/Effect.js
@@ -9,20 +9,41 @@ class Effect {
             [COND_SILENCE, 'silence', 20],
             [COND_DAZZLE, 'dazzle', 15],
             [COND_SLOTH, 'sloth', 15],
-            ['autoheal', 'autoheal', 15],
         ]) {
-            this[char] = game.make.sprite(0, 0, key)
-            this[char].anchor.set(0.5, 0.5)
-            let a = this[char].animations.add('', null, rate)
-            a.onComplete.add(function(s) {
-                s.parent.removeChild(s)
-            })
+            this[char] = this.sprite(0, 0, key, rate)
+        }
+        this.autoheals = []
+        for (let y = 0; y < 2; ++y) {
+            for (let x = 0; x < 2; ++x) {
+                let s = this.sprite(ENEMY_X + ENEMY_WIDTH * (x + 0.5),
+                    ENEMY_Y + ENEMY_HEIGHT * (y + 0.5), 'autoheal', 15)
+                this.autoheals.push(s)
+            }
         }
     }
 
     condition(x, y, char) {
+        if (char === 'autoheal') {
+            for (let s of this.autoheals) {
+                if (s.x === x && s.y === y) {
+                    game.world.addChild(s)
+                    s.animations.play('')
+                }
+            }
+            return
+        }
         this[char].position.set(x, y)
         game.world.addChild(this[char])
         this[char].animations.play('')
+    }
+
+    sprite(x, y, key, rate) {
+        let s = game.make.sprite(x, y, key)
+        s.anchor.set(0.5, 0.5)
+        let a = s.animations.add('', null, rate)
+        a.onComplete.add(function(s) {
+            s.parent.removeChild(s)
+        })
+        return s
     }
 }
